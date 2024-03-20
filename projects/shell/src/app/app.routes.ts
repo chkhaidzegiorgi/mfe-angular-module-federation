4bceb4776da5c7b6a75894e80ce09f4e754385fa
chkhaidzegiorgi <kenjgc@gmail.com>
2024-03-20 18:29:18 +0400

    add mfe support

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -2,20 +2,24 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
+import { loadRemoteModule } from '@angular-architects/native-federation';
+
 export const APP_ROUTES: Routes = [
-    {
-      path: '',
-      component: HomeComponent,
-      pathMatch: 'full'
-    },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'flights',
+    loadComponent: () =>
+      loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 
-    {
-      path: '**',
-      component: NotFoundComponent
-    }
-
-    // DO NOT insert routes after this one.
-    // { path:'**', ...} needs to be the LAST one.
-
+  // DO NOT insert routes after this one.
+  // { path:'**', ...} needs to be the LAST one.
 ];
-
